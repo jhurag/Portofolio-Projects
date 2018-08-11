8d4091d830a072b99dd26aea03e1039881ab5f95
hukyly <hukyly@gmail.com>
2018-08-11 14:59:37 +0200

    First upload.
    Uploading files.

---
+++ Selection_Sort/Selection_Sort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
